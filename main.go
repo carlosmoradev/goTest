ea62458f02d84d376f321c3dd40b1bd8704e57c6
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-07-01 00:56:02 -0500

    little go validation for the first hello world

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var name string
 	fmt.Print("Hola Mundo en Go!!!!!")
+	fmt.Scanf("%s", &name)
+	fmt.Printf("Nuevamente Hola mundo!!! %s.  Excelente!", name)
 }
