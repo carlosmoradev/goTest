10a6d532697dee351198b4e5af6141d71852537d
Carlos Mario Mora Restrepo <github@carlosmora.biz>
2018-07-01 01:42:49 -0500

    Definicion de diferentes formas de declarar variables

--- main.go
+++ main.go
@@ -3,7 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var name string
+	var name string //Forma recomendada para declarar variables. Se especifica el tipo de dato
+	name = "Nombre" //se asigna el valor "Nombre" a la variable name.
+
+	lastName := "<Modificar con el apellido>" //forma corta de declarar variables, sin necesidad de declarar el tipo de dato
+
+	var number = 100 //otra forma de declarar variables sin tipo de dato, pues go automaticamente detecta que tipo de dato es.
+
+	var (
+		a = 1 //Forma de declarar multiples variables de manera simultanea.
+		b = 2
+		c = 3
+	)
+
 	fmt.Print("Ingresa tu nombre: ")
 	fmt.Scanf("%s", &name) //Solicita interaccion del usuario
 	fmt.Printf("Hola %s, bienvenido a go \n", name)
